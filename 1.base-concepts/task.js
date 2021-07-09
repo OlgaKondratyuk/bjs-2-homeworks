dd75b4ecba87dda232aeb3d022b2b2227bccda83
Olga Kondratyuk <kondratyuk.olga@gmail.com>
2021-07-09 20:26:37 +0100

    changes/task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,21 +2,18 @@ function solveEquation(a, b, c) {
   let arr = [];
   let D = b**2-((4*a)*c);
   if (D > 0){
-    let x1 = (-b + Math.sqrt(d) )/(2a);
-    let x2 = (-b - Math.sqrt(d) )/(2a);
+    let x1 = (-b + Math.sqrt(D) )/(2 * a);
+    let x2 = (-b - Math.sqrt(D) )/(2 * a);
     arr[0] = x1;
     arr[1] = x2;
-  }
 } else if (D === 0) {
   let x = -b / (2 * a);
   arr[0] = x;
-} else if (D < 0) {
-  arr[];
 }
   return arr;
 }
 
-solveEquation();
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
