b84861cd69ab47d9cb5c4aa79edc3d57128db249
Olga Kondratyuk <kondratyuk.olga@gmail.com>
2021-08-05 10:42:19 +0100

    changes in task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -22,13 +22,13 @@ function getArrayParams(arr) {
 getArrayParams([1, 2, 3, -100, 10]);
 
 // Задание 2
-function getSumsOfArray(arrOfArr) {
+function worker (arr) {
   let sum = 0;
   let arrSum = [];
-  for (let i = 0; i < arrOfArr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     sum = 0;
-    for (let j = 0; j < arrOfArr[i].length; j++) {
-      sum += arrOfArr[i][j];
+    for (let j = 0; j < arr[i].length; j++) {
+      sum += arr[i][j];
     }
     arrSum[i] = sum;
 
@@ -36,7 +36,9 @@ function getSumsOfArray(arrOfArr) {
   return arrSum;
 }
 
-function calcMax(arr) {
+function mincer(inArr, func) {
+  arr = func(inArr);
+
   let max = -99999;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > max) {
@@ -45,44 +47,13 @@ function calcMax(arr) {
   }
   return max;
 }
-
-let x = ([[1, 2, 3, 4], [5, 6, 7, 8], [10, 20]]);
-//let pista = ([[20,30], [5, 6]]);
-
-let calcSum = getSumsOfArray(x);
-console.log('sumArr: ' + calcSum);
-
-let max = calcMax(calcSum);
-console.log('max: ' + max);
-
+let arrOfArr = ([[1, 2, 3], [4, 5, 6]]);
+  mincer(arrOfArr, worker);
+    arrOfArr = ([[10, 10, 11], [20, 10]]);
+  mincer(arrOfArr, worker);
+    arrOfArr = ([[0, 0, 0], [-1, -100]]);
+  mincer(arrOfArr, worker);
 // Задание 3
-function getSumsOfArray(arrOfArr) {
-  let sum = 0;
-  let arrSum = [];
-  for (let i = 0; i < arrOfArr.length; i++) {
-    sum = 0;
-    for (let j = 0; j < arrOfArr[i].length; j++) {
-      sum += arrOfArr[i][j];
-    }
-    arrSum[i] = sum;
-
-  }
-  return arrSum;
-}
-
-function calcMax(arr) {
-  let max = -99999;
-  let min = 10000;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    };
-    if (arr[i] < min) {
-      min = arr[i];
-    }
-  }
-  return  { min:min, max: max };
-}
 
 function worker2(obj){
   let calcDifference = (obj.max - obj.min);
@@ -91,12 +62,12 @@ function worker2(obj){
 
   return calcDifference;
 }
-
-let x = ([[1, 2, 3, 4], [5, 6, 7, 8], [10, 20]]);
-let calcSum = getSumsOfArray(x);
-//console.log('sumArr: ' + calcSum);
-
 let minMaxObj = calcMax(calcSum);
 
 worker2(minMaxObj);
+
+arrOfArr = ([[-10, -20, -40], [10, 20,30]]);
+mincer(arrOfArr, worker2);
+arrOfArr = ([[0, 0, 0], [-1, -99]]);
+mincer(arrOfArr, worker2);
 //console.log('max: ' + max);
