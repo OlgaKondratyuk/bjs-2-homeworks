e5a789d3037d9eeb3e57237a55c04029107eecdc
Olga Kondratyuk <kondratyuk.olga@gmail.com>
2021-11-03 09:52:58 +0000

    changes in file

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,32 @@
+compareArrays([8, 9], [6]) // false, разные значения
+compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]) // false, разные значения
+compareArrays([9, 2, 4, 8, 2], [9, 2, 4]) // false, разные значения
+compareArrays([1, 2, 3], [2, 3, 1]) // false, разные индексы, хотя и одинаковые значения
+compareArrays([8, 1, 2], [8, 1, 2]); // true
+
 function compareArrays(arr1, arr2) {
   let result;
-
+  result = arr1.every((value, index) => value === arr2[index]);
   // Ваш код
 
   return result; // boolean
 }
 
+
+
 function advancedFilter(arr) {
-  let resultArr;
+  let result;
+  result = arr.filter(item => item >= 0 && item % 3 === 0);
+  result = result.map((item, idx) => {
+    const newItem = item * 10;
+    return newItem
+  });
 
-  // Ваш код
-
-  return resultArr; // array
+  return result;
 }
+advancedFilter([-1,6,-9,3]); // [60,30]
+advancedFilter([-10,-21,12,123]); // [120, 1230]
+advancedFilter([-1,-2]); // []
+
+
+
